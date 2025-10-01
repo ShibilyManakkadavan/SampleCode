e51f0370d2729626bf0a1039ade45e5faee97be0
Shibily <mshibilymkdy@gmail.com>
2025-10-01 23:37:59 +0530

    initial commit

---
+++ App.js
@@ -0,0 +1,16 @@
+//  const heading = React.createElement("h1",{id:"header"},"Hello React from React CDN")
+
+const parent = React.createElement("div",{
+    id:"parent"},
+   [ React.createElement("div",{id:"child1"},
+        [React.createElement("h1",{},"I am a h1 tag-child1"),
+        React.createElement("h2",{},"I am a h2 tag-child1")]
+    ),React.createElement("div",{id:"child2"},
+        [React.createElement("h1",{},"I am a h1 tag-child2"),
+        React.createElement("h2",{},"I am a h2 tag-child2")]
+    ),
+])
+
+ const root = ReactDOM.createRoot(document.getElementById("root"))
+
+ root.render(parent)
