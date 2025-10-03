568606ba34e490192f2f0780282de7e540222942
Shibily <mshibilymkdy@gmail.com>
2025-10-03 22:51:29 +0530

    day 2

--- App.js
+++ App.js
@@ -1,4 +1,6 @@
 //  const heading = React.createElement("h1",{id:"header"},"Hello React from React CDN")
+import React from "react"
+import ReactDOM from 'react-dom/client';
 
 const parent = React.createElement("div",{
     id:"parent"},
